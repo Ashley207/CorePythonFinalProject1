b7fc86b71a418aa0524ed9fc551992a31916879d
Fatmanur Gültekin <ftmangltkn@gmail.com>
2024-08-05 22:38:58 +0300

    update

---
+++ deneme file/final.py
@@ -0,0 +1,13 @@
+#class yapısı olmalı en az 1 tane.
+#en az bir tane karar kontrol yapısı- film önerirken kullanılacak.
+#en az bir tane döngü kullanılmalı
+#kullanıcıdan input alınmalı
+#hata kontrolü sağlanmalı, try-except bloğu
+#en az bir tane değişken tanımlanmalı
+#fonksiyon kullanılmalı
+#import ile başlanacak. 
+#pandas yüklenecek.
+import numpy as np
+import pandas as pd
+data= pd.read_csv("C:\Users\MSI\AppData\Local\Temp\d4555df6-37c6-4893-9a98-a455482a889f_archive.zip.89f\imdb_top_1000.csv")
+data.head(6)
